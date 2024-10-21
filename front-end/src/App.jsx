fb16f47ab13b4b27be83f672eaa4d41397e785aa
nadineadel33 <nadineadel36@gmail.com>
2024-10-20 20:58:50 -0500

    Add profile page with photo upload and user details

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,18 @@
-// src/App.jsx
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import TaskList from './components/TaskList';
+import ProfilePage from './components/ProfilePage';
+import Navbar from './components/Navbar'; 
 import './index.css';
 
 const App = () => {
   return (
     <div className="app-container bg-gray-900 text-white min-h-screen p-6">
-      <TaskList />
+      <Navbar /> {/* Add the Navbar */}
+      <Routes>
+        <Route path="/" element={<TaskList />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 };
